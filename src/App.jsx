20e140eb9d03eec8339b58f4fd2ecbf91411c26f
goesoftwarePY <wpiela9@gmail.com>
2023-11-25 10:49:52 +0100

    components folder added and MyComponent test component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MyCompoment from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1>this is and h1</h1>
-      <h2>this is and h2</h2>
-      <h3>this is and h3</h3>
+      <MyCompoment/>
     </div>
   );
 }
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,3 @@
+export default function MyCompoment(){
+    return <h1>My compoment</h1>
+}
