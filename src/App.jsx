297983d82d5500dfee55a47fa9b8cdc40fc46459
goesoftwarePY <wpiela9@gmail.com>
2023-11-25 10:42:16 +0100

    react app created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is and h1</h1>
+      <h2>this is and h2</h2>
+      <h3>this is and h3</h3>
+    </div>
+  );
+}
+
+export default App;
