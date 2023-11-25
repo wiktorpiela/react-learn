a2d768b71f1d9b7119b1eddc961cba68076423f4
goesoftwarePY <wpiela9@gmail.com>
2023-11-25 10:52:45 +0100

    second component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MyCompoment from './components/MyComponent';
+import SecondComponent from './components/SecondComponent';
 
 function App() {
   return (
     <div className="App">
       <MyCompoment/>
+      <SecondComponent/>
     </div>
   );
 }
---
+++ src/components/SecondComponent.jsx
@@ -0,0 +1,3 @@
+export default function SecondComponent(){
+    return <h2>Second component</h2>
+}
