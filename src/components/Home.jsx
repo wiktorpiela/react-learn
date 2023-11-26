106e8b0d0dde5f257081ebd91392b5c4edcd4c81
goesoftwarePY <wpiela9@gmail.com>
2023-11-26 11:50:46 +0100

    links added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
         <h1>This is Homepage</h1>
+        <Link to="/login">Login</Link>
+        <Link to="/listings">Listings</Link>
     </div>
   )
 }
