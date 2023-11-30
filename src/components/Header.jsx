ddf2d52a5301489b52ba408ef111762608469fbf
goesoftwarePY <wpiela9@gmail.com>
2023-11-30 06:08:59 +0100

    usesing useNavigate added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import { Button, Typography, AppBar, Toolbar } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const propertyBtn = {
+    backgroundColor: 'green',
+    color: 'white',
+    width: '15rem',
+    fontSize: '1.1rem',
+    marginRight: '1rem',
+    '&:hover':{
+      backgroundColor: 'blue'
+    }
+  }
+const loginBtn = {
+    backgroundColor: 'white',
+    color: 'black',
+    width: '15rem',
+    fontSize: '1.1rem',
+    marginLeft: '1rem',
+    '&:hover':{
+        backgroundColor: 'green'
+    }
+}
 
 function Header() {
 
-    const propertyBtn = {
-        backgroundColor: 'green',
-        color: 'white',
-        width: '15rem',
-        fontSize: '1.1rem',
-        marginRight: '1rem',
-        '&:hover':{
-          backgroundColor: 'blue'
-        }
-      }
-    
-    const loginBtn = {
-        backgroundColor: 'white',
-        color: 'black',
-        width: '15rem',
-        fontSize: '1.1rem',
-        marginLeft: '1rem',
-        '&:hover':{
-            backgroundColor: 'green'
-        }
-    }
+    const navigate = useNavigate();
+
+
 
   return (
     <AppBar position="static" sx={{backgroundColor: 'black'}}>
@@ -34,19 +38,19 @@ function Header() {
       }}
       >
       <div>
-        <Button color="inherit"><Typography variant='h4'>LBREP</Typography></Button>
+        <Button color="inherit" onClick={()=>navigate('/')}><Typography variant='h4'>LBREP</Typography></Button>
       </div>
       <div>
-        <Button color="inherit" sx={{marginRight:'2rem'}}>
+        <Button color="inherit" sx={{marginRight:'2rem'}} onClick={()=>navigate('/listings')}>
           <Typography variant='h6'>Listings</Typography>
         </Button>
-        <Button color="inherit" sx={{marginLeft:'2rem'}}>
+        <Button color="inherit" sx={{marginLeft:'2rem'}} onClick={()=>navigate('/')}>
           <Typography variant='h6'>Agencies</Typography>
         </Button>
       </div>
       <div>
         <Button color="inherit" sx={propertyBtn}>Add property</Button>
-        <Button color="inherit" sx={loginBtn}>Login</Button>
+        <Button color="inherit" sx={loginBtn} onClick={()=>navigate('/login')}>Login</Button>
       </div>
     </Toolbar>
   </AppBar>
