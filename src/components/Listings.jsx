88d42caf39e92e931a69fde3fd2bc882dee4a5f3
goesoftwarePY <wpiela9@gmail.com>
2023-11-30 06:21:51 +0100

    react leaflet installed and applied to the project

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet'
 
 function Listings() {
   return (
-    <div>
-        <h1>This is the property listings page</h1>
+    <div style={{height:'100vh'}}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
     </div>
   )
 }
