ca53493e50ab4bdc159324c16b0e2b870bd3b064
goesoftwarePY <wpiela9@gmail.com>
2023-11-30 05:55:04 +0100

    homepage styling added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
 import { Button, Typography, AppBar, Toolbar } from '@mui/material';
+import city from './assets/Prison_Break_205.webp'
 
 function Home() {
-
-
 
   const propertyBtn = {
     backgroundColor: 'green',
@@ -25,6 +24,27 @@ function Home() {
     '&:hover':{
       backgroundColor: 'green'
     }
+  }
+
+  const cityImg = {
+    width: '100%',
+    height: '92vh'
+  }
+
+  const overlatText = {
+    position: 'absolute',
+    zIndex: '100',
+    top: '100px',
+    left: '20px',
+    textAlign: 'center'
+  }
+
+  const homeBtn = {
+    fontSize: '3.5rem',
+    borderRadius: '15px',
+    backgroundColor: 'green',
+    marginTop: '2rem',
+    boxShadow: '3px 3px 3px white'
   }
 
   return (
@@ -53,8 +73,16 @@ function Home() {
           </div>
         </Toolbar>
       </AppBar>
+      <div style={{position: 'relative'}}>
+        <img src={city} alt="pic" style={cityImg}/>
+        <div style={overlatText}>
+          <Typography variant='h1' sx={{color:'white', fontWeight:'bolder', textTransform:'uppercase', userSelect:'none'}}>
+            Find your <span style={{color:'green'}}>next property</span> on the LBPREP website 
+          </Typography>
+          <Button variant='contained' sx={homeBtn}>SEE ALL PROPERTIES</Button>
+        </div>
+      </div>
     </>
-
   )
 }
 
