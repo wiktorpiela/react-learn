187ccb03191a9b553f87b64c56d073a4004b62c5
goesoftwarePY <wpiela9@gmail.com>
2023-12-02 09:19:50 +0100

    listings page layout fixed, map position to sticky

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,21 +1,45 @@
+import { Grid, AppBar, Typography } from '@mui/material'
 import React from 'react'
-import {MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+
 
 function Listings() {
   return (
-    <div style={{height:'100vh'}}>
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-    </div>
+
+    <Grid container>
+
+      <Grid item xs={4}>
+        <Typography variant='h1'>
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </Typography>
+      </Grid>
+
+      <Grid item xs={8}>
+        <AppBar position='sticky'>
+          <div style={{ height: '100vh' }}>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                {/* <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup> */}
+              </Marker>
+            </MapContainer>
+          </div>
+        </AppBar>
+      </Grid>
+    </Grid>
+
+
+
+
+
+
   )
 }
 
